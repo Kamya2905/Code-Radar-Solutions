ddba148c8c7453eb2330cda8930d0d99b6142fe6
Kamya2905 <kamyasharma762@gmail.com>
2025-02-10 09:44:27 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -25,9 +25,12 @@ int main()
         {
             printf("%d\n",a/b);
         }
+       
         break;
         default:
-        printf("error\n");  
+        printf("error\n");
+    
+      
     }
     return 0;
 }
